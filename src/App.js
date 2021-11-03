071c166511bcb155c4626aee3927bf1185050d4f
gaston <gaston_a96@hotmail.com>
2021-11-02 23:34:29 -0300

    Desafio ItemCount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import CartWidget from './components/CartWidget/CartWidget';
+import ItemCount from './components/ItemCount/ItemCount';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './components/ItemCount/ItemCount.css';
 
 function App() {
+
   return (
     <div className="App">
       <NavBar>
         <CartWidget/>
       </NavBar>
       <ItemListContainer greeting="Hola, saludos comprador! :D"/>
+      <ItemCount stock={5} initial={1}/>
     </div>
   );
 }
